c48232058bb6358b451d3e1468f5e47c40e7647e
Sooan L <85142393+yitt1234@users.noreply.github.com>
2025-06-13 10:35:20 +0900

    Update weather.js

--- ashit/weather.js
+++ ashit/weather.js
@@ -1,5 +1,5 @@
 
-const apiKey = '7ce9242fc61fc806b906c5b9538298fe';
+const apiKey = '';
 
 async function getWeather(city) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
